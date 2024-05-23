fe4f30e58bf1941dbfe26336778668fda20edcf4
若叒焫翎 <1184598069@qq.com>
2024-05-23 17:59:35 +0800

    群聊方法新增，聊天室接口可以群聊

--- api/chat_api/chat_group.go
+++ api/chat_api/chat_group.go
@@ -8,6 +8,11 @@ import (
 	"net/http"
 )
 
+//不校验用户是否登录，大家都可以聊天的聊天室
+
+// ConnGroupMap map[连接ip]连接对象
+var ConnGroupMap = map[string]*websocket.Conn{}
+
 // ChatGroupView 聊天的基本框架
 func (ChatApi) ChatGroupView(c *gin.Context) {
 	var upGrader = websocket.Upgrader{
@@ -23,6 +28,8 @@ func (ChatApi) ChatGroupView(c *gin.Context) {
 		res.FailWithCode(res.ArgumentError, c)
 		return
 	}
+	addr := conn.RemoteAddr().String()
+	ConnGroupMap[addr] = conn
 	for {
 		// 消息类型，消息，错误
 		_, msg, err := conn.ReadMessage()
@@ -30,9 +37,18 @@ func (ChatApi) ChatGroupView(c *gin.Context) {
 			// 用户断开聊天
 			break
 		}
-		fmt.Println(string(msg))
+		SendGroupMsg(string(msg))
 		// 发送消息
-		conn.WriteMessage(websocket.TextMessage, []byte("xxx")) //发送消息后对方回复"xxx"
+		//conn.WriteMessage(websocket.TextMessage, []byte("xxx")) //发送消息后对方回复"xxx"
 	}
 	defer conn.Close()
+	delete(ConnGroupMap, addr)
+}
+
+// SendGroupMsg 群聊功能
+func SendGroupMsg(text string) {
+	//用for循环轮询连接对象，给所有连接对象都发送某个用户发送的消息，以达到全局群聊的效果
+	for _, conn := range ConnGroupMap {
+		conn.WriteMessage(websocket.TextMessage, []byte(text))
+	}
 }
